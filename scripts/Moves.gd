56a361871b8d2da1e2655bb05847239505cde4a7
Nate <99703235+GreatNateDev@users.noreply.github.com>
2024-07-25 15:25:44 -0500

    making type advantage system

--- scripts/Moves.gd
+++ scripts/Moves.gd
@@ -1,6 +1,6 @@
 extends Control
 signal text(text,unblock,entity)
-signal damage(entity,damage,text)
+signal damage(entity,damage,text,type)
 signal effects(entity,effect)
 var opp = null
 var critical = 1
@@ -22,27 +22,27 @@ func _on_main_window_attack(Move, Entity, Stats, OStats):
 	match Move:
 		"Slap":
 			if Stats.atk /OStats.def / 2 <= 0:
-				damage.emit(Entity,1 * critical,Entity+" Weakly slapped the "+opp,"weak")
+				damage.emit(Entity,1 * critical,Entity+" Weakly slapped the "+opp,"weak","Normal")
 			else:
-				damage.emit(Entity,Stats.atk * critical /OStats.def / 2,Entity+" Slapped the "+opp,"reg")
+				damage.emit(Entity,Stats.atk * critical /OStats.def / 2,Entity+" Slapped the "+opp,"reg","Normal")
 			anim.play(Entity+"_slap")
 		"Kick":
 			if (Stats.atk -2) /OStats.def <= 0:
-				damage.emit(Entity,1 * critical,Entity+" Weakly kicked the "+opp,"weak")
+				damage.emit(Entity,1 * critical,Entity+" Weakly kicked the "+opp,"weak","Normal")
 			else:
-				damage.emit(Entity,(Stats.atk * critical -2) /OStats.def ,Entity+" Kicked the "+opp,"reg")
+				damage.emit(Entity,(Stats.atk * critical -2) /OStats.def ,Entity+" Kicked the "+opp,"reg","Normal")
 			anim.play(Entity+"_kick") 
 		"Watergun":
 			if Stats.atk /OStats.def <= 0:
-				damage.emit(Entity,1 * critical,Entity+" Weakly used WaterGun on the "+opp,"weak")
+				damage.emit(Entity,1 * critical,Entity+" Weakly used WaterGun on the "+opp,"weak","Water")
 			else:
-				damage.emit(Entity,Stats.atk * critical /OStats.def ,Entity+" Used WaterGun on the "+opp,"reg")
+				damage.emit(Entity,Stats.atk * critical /OStats.def ,Entity+" Used WaterGun on the "+opp,"reg","Water")
 			anim.play(Entity+"_watergun")
 		"Bite":
 			if Stats.atk /OStats.def <= 0:
-				damage.emit(Entity,1 * critical,Entity+" Weakly used Bite on the "+opp,"weak")
+				damage.emit(Entity,1 * critical,Entity+" Weakly used Bite on the "+opp,"weak","Dark")
 			else:
-				damage.emit(Entity,Stats.atk * critical /OStats.def ,Entity+" Bit the "+opp,"reg")
+				damage.emit(Entity,Stats.atk * critical /OStats.def ,Entity+" Bit the "+opp,"reg","Dark")
 			anim.play(Entity+"_bite")
 	critical = 1
 
---
+++ scripts/Types.gd
@@ -0,0 +1,12 @@
+extends Control
+class_name types
+var Normal : Dictionary = {
+	"a_rock": "neff",
+	"a_steel": "neff",
+	"a_ghost": "no",
+	"d_ghost": "no",
+	"d_fighting": "seff",
+}
+var Water : Dictionary = {
+	
+}
