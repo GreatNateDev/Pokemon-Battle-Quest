ad0a49f8e3edb8f6c76873162cad8373bfc5f56e
Nate <99703235+GreatNateDev@users.noreply.github.com>
2024-07-25 15:31:01 -0500

    fire type effects added

--- scripts/Types.gd
+++ scripts/Types.gd
@@ -7,6 +7,22 @@ var Normal : Dictionary = {
 	"d_ghost": "no",
 	"d_fighting": "seff",
 }
-var Water : Dictionary = {
-	
+var Fire : Dictionary = {
+	"a_grass": "seff",
+	"a_ice": "seff",
+	"a_bug": "seff",
+	"a_steel": "seff",
+	"a_fire": "neff",
+	"a_water": "neff",
+	"a_rock": "neff",
+	"a_dragon": "neff",
+	"d_fire": "neff",
+	"d_grass": "neff",
+	"d_ice": "neff",
+	"d_bug": "neff",
+	"d_steel": "neff",
+	"d_fairy": "neff",
+	"d_water": "seff",
+	"d_ground": "seff",
+	"d_rock": "seff",
 }
