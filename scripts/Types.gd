3724e37fee0df4ba5ea0027db7c2719299211800
Nate <99703235+GreatNateDev@users.noreply.github.com>
2024-07-27 11:19:58 -0500

    add full typeing for normal grass water fire

--- scripts/Types.gd
+++ scripts/Types.gd
@@ -1,7 +1,8 @@
 extends Control
 class_name types
 var typx = {
-	"Normal": {"Normal": 1, "Fire": 1, "Water": 1},
-	"Fire": {"Normal": 1, "Fire": .5, "Water": .5},
-	"Water": {"Normal":1, "Fire": 2, "Water": 1}
+	"Normal": {"Normal": 1, "Fire": 1, "Water": 1, "Rock": .5, "Steel": .5, "Ghost": 0, "Electric": 1, "Grass": 1, "Ice": 1, "Fighting": 1, "Poison": 1, "Ground": 1, "Flying": 1, "Psychic": 1,"Bug": 1,"Dragon": 1,"Dark": 1,"Fairy": 1},
+	"Fire": {"Normal": 1, "Fire": .5, "Water": .5, "Rock": .5, "Steel": 2, "Ghost": 1, "Electric": 1, "Grass": 2, "Ice": 2, "Fighting": 1, "Poison": 1, "Ground": 1, "Flying": 1, "Psychic": 1,"Bug": 2,"Dragon": .5,"Dark": 1,"Fairy": 1},
+	"Water": {"Normal": 1, "Fire": 2, "Water": .5, "Rock": 2, "Steel": 1, "Ghost": 1, "Electric": 1, "Grass": .5, "Ice": 1, "Fighting": 1, "Poison": 1, "Ground": 2, "Flying": 1, "Psychic": 1,"Bug": 1,"Dragon": .5,"Dark": 1,"Fairy": 1},
+	"Grass": {"Normal": 1, "Fire": .5, "Water": 2, "Rock": 2, "Steel": .5, "Ghost": 1, "Electric": 1, "Grass": .5, "Ice": 1, "Fighting": 1, "Poison": .5, "Ground": 2, "Flying": .5, "Psychic": 1,"Bug": .5,"Dragon": .5,"Dark": 1,"Fairy": 1}
 }
