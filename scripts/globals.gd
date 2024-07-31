3f7222db6e511083dbc270b63cad996836976d08
Nate <99703235+GreatNateDev@users.noreply.github.com>
2024-07-31 15:12:59 -0500

    shop file fin

--- scripts/globals.gd
+++ scripts/globals.gd
@@ -4,3 +4,4 @@ var loader = false
 var loaded_data = null
 var Enemy_type = null
 var money = 0
+var item :String
--- scripts/shop.gd
+++ scripts/shop.gd
@@ -1,22 +1,52 @@
 extends Control
+var loc_one
+var price_one
+var loc_two
+var price_two
+var loc_three
+var price_three
+var one
+var two
+var three
 func _ready():
-	$Label.text = "Pokedolars: " + str(Globals.money)
+	$Label.text = "Pokedolars: " + str(Globals.money)+"\n press z to skip"
 	var items = itemlist.new()
-	var one = items.list.pick_random()
-	var two = items.list.pick_random()
-	var three = items.list.pick_random()
+	one = items.list.pick_random()
+	two = items.list.pick_random()
+	three = items.list.pick_random()
 	
 	$Panel/TextureButton.texture_normal = load("res://assets/items/" + str(one) + ".png")
 	$Panel2/TextureButton.texture_normal = load("res://assets/items/" + str(two) + ".png")
 	$Panel3/TextureButton.texture_normal = load("res://assets/items/" + str(three) + ".png")
 	
-	var loc_one = items.list.find(one)
-	var price_one = items.prices[loc_one]
-	var loc_two = items.list.find(two)
-	var price_two = items.prices[loc_two]
-	var loc_three = items.list.find(three)
-	var price_three = items.prices[loc_three]
+	loc_one = items.list.find(one)
+	price_one = items.prices[loc_one]
+	loc_two = items.list.find(two)
+	price_two = items.prices[loc_two]
+	loc_three = items.list.find(three)
+	price_three = items.prices[loc_three]
 	
 	$Panel/Label.text = str(price_one) + "$"
 	$Panel2/Label.text = str(price_two) + "$"
 	$Panel3/Label.text = str(price_three) + "$"
+
+
+
+
+func oone():
+	Globals.money -= price_one 
+	Globals.item = one
+	get_tree().change_scene_to_file("res://scenes/main_window.tscn")
+
+func otwo():
+	Globals.money -= price_two
+	Globals.item = two
+	get_tree().change_scene_to_file("res://scenes/main_window.tscn")
+
+func othree():
+	Globals.money -= price_three
+	Globals.item = three
+	get_tree().change_scene_to_file("res://scenes/main_window.tscn")
+func _input(event):
+	if event.is_action_pressed("z"):
+		get_tree().change_scene_to_file("res://scenes/main_window.tscn")
