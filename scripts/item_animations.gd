da4b2f285feb4911cc216ae3b12b48f975f0e4cb
Nate <99703235+GreatNateDev@users.noreply.github.com>
2024-08-02 17:19:34 -0500

    fdsa

--- scripts/item_animations.gd
+++ scripts/item_animations.gd
@@ -14,14 +14,9 @@ func Item_anim(item):
 			var tweene = get_tree().create_tween()
 			tweene.tween_property(res,"rotation",360,1)
 			await get_tree().create_timer(1).timeout
-			var x = enemy.get_rect().size.x
-			x = x / 2
-			var y = enemy.get_rect().size.y
-			y = y / 2
-			print("x: "+str(x)+" "+"y: "+str(y))
 			res.hide()
 			await get_tree().create_timer(.2).timeout
-			res.position = Vector2(x,y)
+			res.position = res.position - Vector2(x,y)
 			res.show()
 			var tweenw = get_tree().create_tween()
 			tweenw.tween_property(res,"position",res.position.y + 20,1)
