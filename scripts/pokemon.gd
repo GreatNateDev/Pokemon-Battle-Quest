6bb74dbdd38f54752cdeec4ad35d3f11cfdcf1f3
Nate <99703235+GreatNateDev@users.noreply.github.com>
2024-08-04 04:47:18 -0500

    added more mons

--- scripts/pokemon.gd
+++ scripts/pokemon.gd
@@ -1,31 +1,27 @@
 extends Control
 class_name pokemon
 signal pkmn_data(pkmn)
-@export var zigzagoon = {
-	"type": "Normal",
-	"type2": "none",
-	"catch" : 255
-}
-@export var mudkip = {
-	"type" : "Water",
-	"type2": "none",
-	"catch" : 45
-}
-@export var torchic = {
-	"type" : "Fire",
-	"type2": "none",
-	"catch" : 45
-}
-@export var treecko = {
-	"type" : "Grass",
-	"type2": "none",
-	"catch" : 45
-}
-@export var pkmn = {
+var totodile = {"type": "Water", "type2": "none", "catch": 45}
+var bulbasaur = {"type": "Grass", "type2": "Poison", "catch": 45}
+var charmander = {"type": "Fire", "type2": "none", "catch": 45}
+var squirtle = {"type": "Water", "type2": "none", "catch": 45}
+var cyndaquil = {"type": "Fire", "type2": "none", "catch": 45}
+var chikorita = {"type": "Grass", "type2": "none", "catch": 45}
+var zigzagoon = {"type": "Normal", "type2": "none", "catch" : 255}
+var mudkip = {"type" : "Water", "type2": "none", "catch" : 45}
+var torchic = {"type" : "Fire", "type2": "none", "catch" : 45}
+var treecko = {"type" : "Grass","type2": "none","catch" : 45}
+var pkmn = {
 	"zigzagoon": zigzagoon,
 	"treecko": treecko,
 	"mudkip": mudkip,
 	"torchic": torchic,
+	"totodile": totodile,
+	"bulbasaur":bulbasaur,
+	"charmander":charmander,
+	"squirtle": squirtle,
+	"cyndaquil": cyndaquil,
+	"chikorita": chikorita,
 }
 
 func _on_main_window_type_requester(pokemoner):
