87861b19efb918e1abb51b0af3fb784c0ac47c22
Nate <99703235+GreatNateDev@users.noreply.github.com>
2024-08-03 23:10:24 -0500

    made shop scaleable

--- scripts/shop.gd
+++ scripts/shop.gd
@@ -9,15 +9,15 @@ var one
 var two
 var three
 func _ready():
-	$Label.text = "Pokedolars: " + str(Globals.money)+"\n press z to skip"
+	$Label/Label.text = "Pokedolars: " + str(Globals.money)+"\n press z to skip"
 	var items = itemlist.new()
 	one = items.list.pick_random()
 	two = items.list.pick_random()
 	three = items.list.pick_random()
 	
-	$Panel/TextureButton.texture_normal = load("res://assets/items/" + str(one) + ".png")
-	$Panel2/TextureButton.texture_normal = load("res://assets/items/" + str(two) + ".png")
-	$Panel3/TextureButton.texture_normal = load("res://assets/items/" + str(three) + ".png")
+	$"Panel one/Panel/TextureButton".texture_normal = load("res://assets/items/" + str(one) + ".png")
+	$"Panel two/Panel2/TextureButton".texture_normal = load("res://assets/items/" + str(two) + ".png")
+	$"Panel three/Panel3/TextureButton".texture_normal = load("res://assets/items/" + str(three) + ".png")
 	
 	loc_one = items.list.find(one)
 	price_one = items.prices[loc_one]
@@ -26,9 +26,9 @@ func _ready():
 	loc_three = items.list.find(three)
 	price_three = items.prices[loc_three]
 	
-	$Panel/Label.text = str(price_one) + "$"
-	$Panel2/Label.text = str(price_two) + "$"
-	$Panel3/Label.text = str(price_three) + "$"
+	$"Panel one/Panel/Lable".text = str(price_one) + "$"
+	$"Panel two/Panel2/Lable".text = str(price_two) + "$"
+	$"Panel three/Panel3/Lable".text = str(price_three) + "$"
 	Globals.back_shop = true
 
 
