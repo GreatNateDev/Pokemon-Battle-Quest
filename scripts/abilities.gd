df0728534590b830bf7ae51ac457cb0c931f4cc9
Nate <99703235+GreatNateDev@users.noreply.github.com>
2024-08-08 20:10:27 -0500

    start adding abilitys

---
+++ scripts/abilities.gd
@@ -0,0 +1,4 @@
+extends Control
+signal extend_damage()
+signal extend_weather()
+signal extend_other()
--- scripts/pokemon.gd
+++ scripts/pokemon.gd
@@ -3,24 +3,25 @@ class_name pokemon
 signal pkmn_data()
 signal return_mon_data()
 signal random_pokemon()
-var totodile = {"type": "Water", "type2": "none", "catch": 45, "base_stats": {"hp": 50, "atk": 65, "def": 64, "spd": 43}}
-var bulbasaur = {"type": "Grass", "type2": "Poison", "catch": 45, "base_stats": {"hp": 45, "atk": 49, "def": 49, "spd": 45}}
-var charmander = {"type": "Fire", "type2": "none", "catch": 45, "base_stats": {"hp": 39, "atk": 52, "def": 43, "spd": 65}}
-var squirtle = {"type": "Water", "type2": "none", "catch": 45, "base_stats": {"hp": 44, "atk": 48, "def": 65, "spd": 43}}
-var cyndaquil = {"type": "Fire", "type2": "none", "catch": 45, "base_stats": {"hp": 39, "atk": 52, "def": 43, "spd": 65}}
-var chikorita = {"type": "Grass", "type2": "none", "catch": 45, "base_stats": {"hp": 45, "atk": 49, "def": 65, "spd": 45}}
-var zigzagoon = {"type": "Normal", "type2": "none", "catch": 255, "base_stats": {"hp": 38, "atk": 30, "def": 41, "spd": 60}}
-var mudkip = {"type": "Water", "type2": "none", "catch": 45, "base_stats": {"hp": 50, "atk": 70, "def": 50, "spd": 40}}
-var torchic = {"type": "Fire", "type2": "none", "catch": 45, "base_stats": {"hp": 45, "atk": 60, "def": 40, "spd": 45}}
-var treecko = {"type": "Grass", "type2": "none", "catch": 45, "base_stats": {"hp": 40, "atk": 45, "def": 35, "spd": 70}}
+var totodile = {"type": "Water", "type2": "none", "catch": 45, "base_stats": {"hp": 50, "atk": 65, "def": 64, "spd": 43}, "ability": "Torrent"}
+var bulbasaur = {"type": "Grass", "type2": "Poison", "catch": 45, "base_stats": {"hp": 45, "atk": 49, "def": 49, "spd": 45}, "ability": "Overgrow"}
+var charmander = {"type": "Fire", "type2": "none", "catch": 45, "base_stats": {"hp": 39, "atk": 52, "def": 43, "spd": 65}, "ability": "Blaze"}
+var squirtle = {"type": "Water", "type2": "none", "catch": 45, "base_stats": {"hp": 44, "atk": 48, "def": 65, "spd": 43}, "ability": "Torrent"}
+var cyndaquil = {"type": "Fire", "type2": "none", "catch": 45, "base_stats": {"hp": 39, "atk": 52, "def": 43, "spd": 65}, "ability": "Blaze"}
+var chikorita = {"type": "Grass", "type2": "none", "catch": 45, "base_stats": {"hp": 45, "atk": 49, "def": 65, "spd": 45}, "ability": "Overgrow"}
+var zigzagoon = {"type": "Normal", "type2": "none", "catch": 255, "base_stats": {"hp": 38, "atk": 30, "def": 41, "spd": 60}, "ability": "Pickup"}
+var mudkip = {"type": "Water", "type2": "none", "catch": 45, "base_stats": {"hp": 50, "atk": 70, "def": 50, "spd": 40}, "ability": "Torrent"}
+var torchic = {"type": "Fire", "type2": "none", "catch": 45, "base_stats": {"hp": 45, "atk": 60, "def": 40, "spd": 45}, "ability": "Blaze"}
+var treecko = {"type": "Grass", "type2": "none", "catch": 45, "base_stats": {"hp": 40, "atk": 45, "def": 35, "spd": 70}, "ability": "Overgrow"}
+
 var pkmn = {
 	"zigzagoon": zigzagoon,
 	"treecko": treecko,
 	"mudkip": mudkip,
 	"torchic": torchic,
 	"totodile": totodile,
-	"bulbasaur":bulbasaur,
-	"charmander":charmander,
+	"bulbasaur": bulbasaur,
+	"charmander": charmander,
 	"squirtle": squirtle,
 	"cyndaquil": cyndaquil,
 	"chikorita": chikorita,
