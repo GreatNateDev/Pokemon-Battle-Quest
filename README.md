d733bb4262ed2718caf6067cc8b50016467e0924
Nate <99703235+GreatNateDev@users.noreply.github.com>
2024-08-15 00:28:05 +0000

    fully added not much of a diff till i add stat changing moves though

--- README.md
+++ README.md
@@ -34,7 +34,7 @@ This should just run in any godot editor 4.3x
 Make a pull request with the feature(s) you have made!
 
 ## TODO:
-physical/special split\
+add stat changing moves/ abilitys\
 add trainers all the way up to champion\
 add all abilities from gen 3\
 add all items from gen 3\
--- scripts/Moves.gd
+++ scripts/Moves.gd
@@ -1,18 +1,19 @@
 extends Control
 class_name Movos
-func create_move(power, accuracy, move_type, text, status, c):
+func create_move(power, accuracy, move_type, text, status, c, catagory):
 	return {
 		"power": power,
 		"accuracy": accuracy,
 		"type": move_type,
 		"text": text,
 		"status": status,
-		"c": c
+		"c": c,
+		"catagory": catagory, 
 	}
 var movs = {
-	"Watergun": create_move(40, 100, "Water", " was sprayed with water!",false,null),
-	"Bite": create_move(60, 100, "Dark", " was bit!",false,null),
-	"Ember": create_move(40, 100, "Fire", " was burned by embers!",false,null),
-	"Absorb": create_move(20, 100, "Grass", " had its health absorbed!",false,null),
-	"Tackle": create_move(40, 100, "Normal", " was tackled!",false,null),
+	"Watergun": create_move(40, 100, "Water", " was sprayed with water!",false,null,"Special"),
+	"Bite": create_move(60, 100, "Dark", " was bit!",false,null,"Physical"),
+	"Ember": create_move(40, 100, "Fire", " was burned by embers!",false,null,"Special"),
+	"Absorb": create_move(20, 100, "Grass", " had its health absorbed!",false,null,"Special"),
+	"Tackle": create_move(40, 100, "Normal", " was tackled!",false,null,"Physical"),
 }
