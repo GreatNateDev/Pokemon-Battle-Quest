af090dd1032c9eaffb82687a3c17045483660588
Nate <Nate@pelmel.net>
2024-11-11 21:03:04 -0600

    Start overhall this will take a bit

---
+++ scripts/Main_Menu.gd
@@ -0,0 +1,37 @@
+
+
+
+
+func charmander() -> void:
+	pass # Replace with function body.
+
+
+func squirtle() -> void:
+	pass # Replace with function body.
+
+
+func bulbasaur() -> void:
+	pass # Replace with function body.
+
+
+func cyndaquil() -> void:
+	pass # Replace with function body.
+
+
+func tododile() -> void:
+	pass # Replace with function body.
+
+
+func chikorita() -> void:
+	pass # Replace with function body.
+
+
+func torchic() -> void:
+	pass # Replace with function body.
+
+
+func mudkip() -> void:
+	pass # Replace with function body.
+
+func treecko() -> void:
+	pass # Replace with function body.
