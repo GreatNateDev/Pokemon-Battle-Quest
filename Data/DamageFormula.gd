e15634a3b1c77fd1c7b742a57dc8f635e1b992b1
Nate <Nate@pelmel.net>
2024-11-12 19:26:08 -0600

    basic damage formula missing some things

--- Data/DamageFormula.gd
+++ Data/DamageFormula.gd
@@ -1,36 +1,27 @@
 extends Control
+#This will need more work
 class_name DamageFormula
 var opp
-#FIX SPACES
+var Type = preload("res://Data/Types.gd").new()
 func Attack(Move, Entity, Stats, OStats):
-	return 1
 	if Entity == "Player": opp = "Enemy"
 	else : opp = "Player"
 	var f = FileAccess.open("res://Data/Moves.json", FileAccess.READ)
 	var json = JSON.parse_string(f.get_as_text())
 	f.close()
 	var mov = json[Move]
-	print(mov)
-	#var type_effectiveness = getMultiplier(mov.type,OStats.type,OStats.type2)
-	var hit_chance = randi() % 100 + 1 
-	var damager
-	if hit_chance > mov.accuracy:
-		return 0
-	if mov.catagory == "Physical":
-		damager = Stats.atk
-	if mov.catagory == "Special":
-		damager = Stats.spatk
+	var type_effectiveness = getMultiplier(mov.type,OStats.type1,OStats.type2)
 	var random_number = randi() % 16 + 85
-	var base_damage = ((2 * Stats.level / 5 + 2) * damager * mov.power / OStats.def) / 50 + 2
-	var adjusted_damage = base_damage #* stab #* type_effectiveness
+	var base_damage = ((2 * Stats.level / 5 + 2)  * mov.power / OStats.defense) / 50 + 2
+	var adjusted_damage = base_damage * type_effectiveness
 	var final_damage = adjusted_damage * random_number / 100
-	#return damage
-#func getMultiplier(move_type, primary_type, secondary_type):
-	#var multiplier = 1
-	#if move_type in Type.typx:
-	#	if primary_type in Type.typx[move_type]:
-	#		multiplier *= Type.typx[move_type][primary_type]
-	#	if secondary_type in Type.typx[move_type]:
-		#	multiplier *= Type.typx[move_type][secondary_type]
+	return final_damage
+func getMultiplier(move_type, primary_type, secondary_type):
+	var multiplier = 1
+	if move_type in Type.typx:
+		if primary_type in Type.typx[move_type]:
+			multiplier *= Type.typx[move_type][primary_type]
+		if secondary_type in Type.typx[move_type]:
+			multiplier *= Type.typx[move_type][secondary_type]
 
-	#return multiplier
+	return multiplier
